384d8ccbc10123daa849aa961bb46db1381c3c0a
mb0390231 <michaelbroyles68@gmail.com>
2023-10-20 23:55:01 -0500

    object parser

---
+++ highlevel_sdk/object_parser.py
@@ -0,0 +1,26 @@
+from highlevel_sdk.exceptions import HighLevelError
+from highlevel_sdk.models.abstract_object import AbstractObject
+
+
+class ObjectParser(object):
+    def parse_single(response, target_class):
+        if not target_class:
+            raise HighLevelError("Must specify target class when parsing single object")
+
+        if isinstance(response, dict):
+            return AbstractObject.create_object(response, target_class)
+        else:
+            raise HighLevelError("Must specify either target class calling object")
+
+    def parse_multiple(response, target_class=None):
+        ret = []
+        for key in response.keys():
+            if key == "meta" or key == "traceId":
+                continue
+
+            if isinstance(response[key], list):
+                for json_obj in response[key]:
+                    ret.append(ObjectParser.parse_single(json_obj, target_class))
+            else:
+                ret.append(ObjectParser.parse_single(response[key], target_class))
+        return ret
